da381ef975727f2c0d5a68033139f049e7f4b926
Octavia Isom <55517177+octaviaisom@users.noreply.github.com>
2020-03-10 18:11:14 -0400

    Reorganized and consolidated cleaning code

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE top_songs (
+rank INT PRIMARY KEY,
+apple TEXT,
+billboard TEXT,
+spotify TEXT,
+);
+
+CREATE TABLE top_artist (
+rank INT PRIMARY KEY,
+apple TEXT,
+billboard TEXT,
+spotify TEXT,
+);
