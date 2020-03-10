8a408452c242987fea9b811f4aa9df3b448a4035
Octavia Isom <55517177+octaviaisom@users.noreply.github.com>
2020-03-10 19:17:27 -0400

    Consolidation complete

--- schema.sql
+++ schema.sql
@@ -2,12 +2,12 @@ CREATE TABLE top_songs (
 rank INT PRIMARY KEY,
 apple TEXT,
 billboard TEXT,
-spotify TEXT,
+spotify TEXT
 );
 
-CREATE TABLE top_artist (
+CREATE TABLE artists (
 rank INT PRIMARY KEY,
 apple TEXT,
 billboard TEXT,
-spotify TEXT,
+spotify TEXT
 );
